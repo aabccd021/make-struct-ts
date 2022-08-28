961d22e6c186600b6c3f238842122a762d6f1435
aabccd021 <aabccd021@gmail.com>
2022-08-28 13:12:37 +0700

    fix(index.ts): refactor typescript types

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import { pipe } from 'fp-ts/lib/function';
 import * as Record from 'fp-ts/Record';
 import * as t from 'io-ts';
 
+type ValueOf<T> = T[keyof T];
+
 export const make: <P extends t.Props, T extends t.TypeC<P>, R = t.TypeOf<T>>(
   t: T
 ) => {
@@ -9,5 +11,10 @@ export const make: <P extends t.Props, T extends t.TypeC<P>, R = t.TypeOf<T>>(
 } = (def) =>
   pipe(
     def.props,
-    Record.mapWithIndex((key) => (a) => (b) => ({ ...a, [key]: b }))
+    Record.mapWithIndex(
+      (key) => (a: t.TypeOf<t.TypeC<t.Props>>) => (b: ValueOf<t.TypeOf<t.TypeC<t.Props>>>) => ({
+        ...a,
+        [key]: b,
+      })
+    )
   ) as any;
